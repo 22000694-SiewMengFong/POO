dd780fc9ba6b1dc13a26a8200187020e316db30f
mf <mf@LAPTOP-5IJ7RKP1>
2023-05-30 23:15:47 +0800

    Added comments and try catch instead of IF

--- POO/Test/CalculatorTest.java
+++ POO/Test/CalculatorTest.java
@@ -1,5 +1,4 @@
 import static org.junit.Assert.*;
-
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
@@ -13,18 +12,11 @@ public class CalculatorTest {
 	public static void setUpBeforeClass() throws Exception {
 	}
 
-	@AfterClass
-	public static void tearDownAfterClass() throws Exception {
-	}
-
 	@Before
 	public void setUp() throws Exception {
 	}
-
-	@After
-	public void tearDown() throws Exception {
-	}
-
+	
+	// Test add
 	@Test
 	public void testAdd() {
 		int a = 4321;
@@ -37,6 +29,8 @@ public class CalculatorTest {
 		assertEquals(expected, actual);
 	}
 
+	
+	// Test subtract
 	@Test
 	public void testSubtract() {
 		int a = 1000;
@@ -49,6 +43,7 @@ public class CalculatorTest {
 		assertEquals(expected, actual);
 	}
 
+	// Test multiply
 	@Test
 	public void testMultiply() {
 		int a = 3;
@@ -61,18 +56,28 @@ public class CalculatorTest {
 		assertEquals(expected, actual);
 	}
 
+	// Test if error is thrown if divided by 0
 	@Test
 	public void testDivideZero() {
 		int a = 1000;
 		int b = 0;
 
 		Calculator cal = new Calculator();
-		int actual = cal.divide(a, b);
-
-		int expected = 0;
-		assertEquals(expected, actual);
+		
+		boolean expected = false;
+		
+		// Check if arithmetic error is throw
+		try {
+			cal.divide(a, b);
+		} catch(ArithmeticException e) {
+			expected = true;
+		}
+		
+		assertTrue(expected);
+		
 	}
 	
+	// Test normal divide
 	@Test
 	public void testDivide() {
 		int a = 1000;
@@ -83,6 +88,14 @@ public class CalculatorTest {
 
 		int expected = 250;
 		assertEquals(expected, actual);
+	}
+	
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
 	}
 
 }
--- POO/src/Calculator.java
+++ POO/src/Calculator.java
@@ -1,24 +1,31 @@
 
-
 public class Calculator {
-	
+
+	// Add a and b
 	public int add(int a, int b) {
 		return a + b;
 	}
 
+	// Subtract a by b
 	public int subtract(int a, int b) {
 		return a - b;
 	}
 
+	// Multiply a by b
 	public int multiply(int a, int b) {
 		return a * b;
 	}
 
+	// Divide a by b and catch math error
 	public int divide(int a, int b) {
-		if (b == 0) {
-			return 0;
+
+		try {
+			return a / b;
+			
+		} catch (ArithmeticException e) {
+			throw new ArithmeticException();
 		}
-		return a / b;
+
 	}
 
 }
